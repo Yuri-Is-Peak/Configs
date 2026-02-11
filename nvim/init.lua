6bfab54234a742cc0e768717ced1771297f7ea4a
YuriIsPeak <Yuriispeak@proton.me>
2026-02-11 23:49:55 +0200

    (NVIM) Changes to startup.nvim and mason

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,3 +3,11 @@ vim.o.timeout = true
 vim.o.timeoutlen = 500
 require("config.lazy")
 require("Clover")
+require("Clover.set")
+vim.cmd([[
+  highlight Normal guibg=none
+  highlight NonText guibg=none
+  highlight NormalNC guibg=none
+  highlight EndOfBuffer guibg=none
+]])
+
---
+++ nvim/lua/plugins/mason.lua
@@ -0,0 +1,67 @@
+return {
+  -- Mason: installs LSP servers
+  {
+    "williamboman/mason.nvim",
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  -- Mason ↔ Neovim LSP bridge
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = { "pyright" },
+      })
+    end,
+  },
+
+  -- Completion engine
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+    },
+    config = function()
+      local cmp = require("cmp")
+
+      cmp.setup({
+        mapping = cmp.mapping.preset.insert({
+          ["<C-Space>"] = cmp.mapping.complete(),
+          ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        }),
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer" },
+          { name = "path" },
+        },
+      })
+    end,
+  },
+
+  -- LSP (NEW API)
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason-lspconfig.nvim",
+      "hrsh7th/cmp-nvim-lsp",
+    },
+    config = function()
+      local capabilities =
+        require("cmp_nvim_lsp").default_capabilities()
+
+      -- Define config using the new API
+      vim.lsp.config("pyright", {
+        capabilities = capabilities,
+      })
+
+      -- Enable it
+      vim.lsp.enable("pyright")
+    end,
+  },
+}
+
